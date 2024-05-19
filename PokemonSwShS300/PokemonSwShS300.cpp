ef134ee284b637ed94d1c3b7f0de41d456247cfa
ShebetaNickolay <n.shebeta@uniyar.ac.ru>
2024-05-19 11:58:09 +0300

    Удаление файлов и пример с использованием SFML
    Удаление файлов и пример с использованием SFML

--- PokemonSwShS300/PokemonSwShS300.cpp
+++ PokemonSwShS300/PokemonSwShS300.cpp
@@ -1,13 +1,30 @@
-﻿#include "Head.h"
-#include "Classes.h"
-#include "Graph2.h"
-#include <iostream>
+﻿#include <SFML/Graphics.hpp>
 
-using namespace std;
-int main(int argc, char** argv) {
-    menu(argc, argv);
-    cout << 1;
-    cout << 2;
-    cout << 3;
+using namespace sf;
+
+int main()
+{
+
+    RenderWindow window(VideoMode(400, 400), L"Новый проект", Style::Default);
+
+    window.setVerticalSyncEnabled(true);
+
+    CircleShape shape(100.f, 3);
+    shape.setPosition(100, 100);
+    shape.setFillColor(Color::Magenta);
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear(Color::Blue);
+        window.draw(shape);
+        window.display();
+    }
+    return 0;
 }
-
