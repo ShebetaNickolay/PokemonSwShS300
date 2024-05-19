2c26f0a0042e215656f0b12fa00274af1e212bf7
ShebetaNickolay <n.shebeta@uniyar.ac.ru>
2024-05-19 11:32:50 +0300

    Update PokemonSwShS300.cpp

--- PokemonSwShS300/PokemonSwShS300.cpp
+++ PokemonSwShS300/PokemonSwShS300.cpp
@@ -1,8 +1,12 @@
 ﻿#include "Head.h"
 #include "Classes.h"
 #include "Graph2.h"
+#include <iostream>
 
+using namespace std;
 int main(int argc, char** argv) {
     menu(argc, argv);
+    cout << 1;
+    cout << 2;
 }
 
